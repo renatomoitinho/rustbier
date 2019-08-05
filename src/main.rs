d6ead6a3b911fa7f6859b274bb80fceb889b7a9e
Augusto César Dias <augusto.dias@olx.com>
2019-08-05 12:42:43 +0200

    changing query string deserializer to non strict mode

--- src/main.rs
+++ src/main.rs
@@ -25,67 +25,69 @@ use futures::future::{join_all, Future};
 use image_processor::*;
 use magick_rust::magick_wand_genesis;
 use rusoto_s3::S3Client;
-use serde_qs::actix::QsQuery;
 use std::env;
 use std::sync::{Arc, Once};
 
 static START: Once = Once::new();
 
 fn index(
-    _req: HttpRequest,
+    req: HttpRequest,
     path: web::Path<String>,
-    query: QsQuery<ProcessImageRequest>,
+    qs_config: web::Data<serde_qs::Config>,
     s3_client: web::Data<S3Client>,
     config: web::Data<Configuration>,
 ) -> impl Future<Item = HttpResponse, Error = actix_web::Error> {
-    debug!("Request parameters: {:?}", query);
+    let rs_query = qs_config.deserialize_str::<ProcessImageRequest>(req.query_string()).map_err(|e| actix_web::error::ErrorBadRequest(e));
+    futures::done(rs_query).and_then(move |query| {
+        debug!("Request parameters: {:?}", query);
 
-    let ProcessImageRequest {
-        size,
-        format,
-        quality,
-        watermarks,
-        rotation,
-    } = query.into_inner();
-    let bucket = Arc::new(config.bucket.clone());
-    let bucket_cp = bucket.clone();
-    let format1 = Arc::new(format);
-    let format2 = format1.clone();
-    let format3 = format1.clone();
-    let s3_client_cp = s3_client.clone();
-    let wm_futures = watermarks
-        .clone()
-        .into_iter()
-        .map(move |wm| s3::get_image(&s3_client_cp, &bucket_cp, &wm.filename));
-    s3::get_image(&s3_client, &bucket, &path)
-        .map(move |body| {
-            pre_process_image(
-                &body[..],
-                &rotation,
-                &size,
-                &format1,
-                quality,
-                config.png_quality,
-            )
-            .map_err(|e| {
-                error!("Error processing image: {:?}", e);
-                actix_web::error::ErrorInternalServerError(e)
+        let ProcessImageRequest {
+            size,
+            format,
+            quality,
+            watermarks,
+            rotation,
+        } = query;
+        let bucket = Arc::new(config.bucket.clone());
+        let bucket_cp = bucket.clone();
+        let format1 = Arc::new(format);
+        let format2 = format1.clone();
+        let format3 = format1.clone();
+        let s3_client_cp = s3_client.clone();
+        let wm_futures = watermarks
+            .clone()
+            .into_iter()
+            .map(move |wm| s3::get_image(&s3_client_cp, &bucket_cp, &wm.filename));
+        s3::get_image(&s3_client, &bucket, &path)
+            .map(move |body| {
+                pre_process_image(
+                    &body[..],
+                    &rotation,
+                    &size,
+                    &format1,
+                    quality,
+                    config.png_quality,
+                )
+                .map_err(|e| {
+                    error!("Error processing image: {:?}", e);
+                    actix_web::error::ErrorInternalServerError(e)
+                })
             })
-        })
-        .map(move |body| {
-            join_all(wm_futures).map(move |buffers| {
-                buffers
-                    .iter()
-                    .zip(watermarks)
-                    .fold(body, move |current, item| {
-                        let (wm_buffer, wm) = item;
-                        apply_watermark(&current?, &wm_buffer[..], &wm, &format2)
-                            .map_err(|e| e.into())
-                    })
+            .map(move |body| {
+                join_all(wm_futures).map(move |buffers| {
+                    buffers
+                        .iter()
+                        .zip(watermarks)
+                        .fold(body, move |current, item| {
+                            let (wm_buffer, wm) = item;
+                            apply_watermark(&current?, &wm_buffer[..], &wm, &format2)
+                                .map_err(|e| e.into())
+                        })
+                })
             })
+            .flatten()
+            .and_then(move |res| index_response(res, &format3))
         })
-        .flatten()
-        .and_then(move |res| index_response(res, &format3))
 }
 
 fn index_response(
@@ -131,6 +133,9 @@ fn main() -> std::io::Result<()> {
     let prometheus = PrometheusMetrics::new(name, "/metrics");
     let s3 = S3Client::new(config_data.region.clone());
     let s3_client_data = web::Data::new(s3);
+    //accept url encoded with brackets or their encoded equivalents
+    let qs_config = serde_qs::Config::new(5, false);
+    let qs_config_data = web::Data::new(qs_config);
 
     Server::build()
         .bind(
@@ -140,6 +145,7 @@ fn main() -> std::io::Result<()> {
                 HttpService::build().keep_alive(KeepAlive::Os).h1(App::new()
                     .register_data(s3_client_data.clone())
                     .register_data(config_data.clone())
+                    .register_data(qs_config_data.clone())
                     .wrap(prometheus.clone())
                     .wrap(actix_web::middleware::Logger::default())
                     .service(web::resource("/health").route(web::get().to(health)))
